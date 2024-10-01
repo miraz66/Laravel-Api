02479f910b3c51f5cd876ca5834e225330b9320d
miraz66 <mirajulislam0509@gmail.com>
2024-10-01 11:52:06 +0600

    User Token Create

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,51 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\LoginUserRequest;
-use Illuminate\Database\Console\Migrations\StatusCommand;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Traits\ApiResponses; // Ensure this is at the top of your class
 
 class AuthController extends Controller
 {
+    // Using the trait globally within the controller
+    use ApiResponses;
+
     public function login(LoginUserRequest $request)
     {
+        // Validate the incoming request data
         $request->validated($request->all());
 
+        // Attempt to authenticate the user
         if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(
-                [
-                    'email' => $request->get('email'),
-                    'message' => 'Login failed',
-                ],
-                401
-            );
+            return $this->error('Invalid credentials', 401);
         }
 
+        // Retrieve the authenticated user by email
         $user = User::firstWhere('email', $request->email);
 
-        return response()->json(
-            [
-                'email' => $request->get('email'),
-                'message' => 'Login successful',
-                'user' => $user,
-                'password' => $request->get('password'),
-                'token' => $user->createToken('remember_token', $request->email)->plainTextToken,
-            ],
-            200
-        );
-    }
-
-    public function register(Request $request)
-    {
-        return response()->json(
-            [
-                'email' => $request->get('email'),
-                'message' => 'Register successful',
-                'password' => $request->get('password'),
-            ],
-            200
+        // Return a successful response with user information
+        return $this->ok(
+            'Authenticated',
+            ['token' => $user->createToken('Api token for ' . $request->email)->plainTextToken]
         );
     }
 }
---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponses
+{
+  protected function ok($message, $data)
+  {
+    return $this->success($message, $data, 200);
+  }
+
+  protected function success($message, $data, $code = 200)
+  {
+    return response()->json(['message' => $message, 'data' => $data, 'status' => $code,]);
+  }
+
+  protected function error($message, $statusCode = 404)
+  {
+    return response()->json(['error' => $message], $statusCode);
+  }
+}
